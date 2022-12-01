9a725ced23a4b138d2af0bb703cb0b1ada173cfc
Bhoomika Madhukar <bhoomikamadhukar@Bhoomikas-MacBook-Pro.local>
2022-12-01 16:24:40 -0500

    Add functionality to view all users from admin page

---
+++ src/components/admin/index.js
@@ -0,0 +1,32 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import * as userService from "../../services/users-service";
+import React from "react";
+import {UserList} from "../profile/user-list";
+
+
+export const Users = () => {
+    const [existingUsers, setExistingUsers] = useState([]);
+
+
+    const navigate = useNavigate();
+
+    const deleteUser = (uid) =>
+        userService.deleteUser(uid)
+            .then(findAllUsers)
+    const findAllUsers = () =>
+        userService.findAllUsersAsAdmin()
+            .then(users => {
+                setExistingUsers(users)
+            })
+    useEffect(findAllUsers, []);
+
+    return (
+        <div>
+          <h1>Existing Users</h1>
+
+          <UserList users={existingUsers} deleteUser={deleteUser}/>
+
+        </div>
+    );
+};
