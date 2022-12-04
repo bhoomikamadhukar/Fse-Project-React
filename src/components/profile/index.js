55c5947d1de2f337115eef50e5aeb6111b392090
nikithajain888 <nikithajain888@github.com>
2022-12-03 21:09:54 -0500

    logic for checking user role

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,14 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import MyTuits from "./my-tuits";
-import {HashRouter, Link, Route, Routes, useNavigate, useLocation} from "react-router-dom";
+import { HashRouter, Link, Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import * as service from "../../services/auth-service"
 import MyLikes from "./my-likes";
 import MyDislikes from "./my-dislikes";
+import UserList from "./user-list";
+
 
 const Profile = () => {
   const navigate = useNavigate();
   const location = useLocation();
- 
+
   const [profile, setProfile] = useState({});
   useEffect(async () => {
     try {
@@ -20,10 +22,10 @@ const Profile = () => {
   }, []);
   const logout = () => {
     service.logout()
-        .then(() => navigate('/login'));
+      .then(() => navigate('/login'));
   }
-  
-  return(
+
+  return (
     <div className="ttr-profile">
       <div className="border border-bottom-0">
         <h4 className="p-2 mb-0 pb-0 fw-bolder">
@@ -31,15 +33,15 @@ const Profile = () => {
           <i className="fa fa-badge-check text-primary"></i></h4>
         <span className="ps-2">67.6K Tuits</span>
         <div className="mb-5 position-relative">
-          <img className="w-100" src="../images/nasa-profile-header.jpg"/>
+          <img className="w-100" src="../images/nasa-profile-header.jpg" />
           <div className="bottom-0 left-0 position-absolute">
             <div className="position-relative">
               <img className="position-relative ttr-z-index-1 ttr-top-40px ttr-width-150px"
-                   src="../images/nasa-3.png"/>
+                src="../images/nasa-3.png" />
             </div>
           </div>
           <Link to="/profile/edit"
-                className="mt-2 me-2 btn btn-large btn-light border border-secondary fw-bolder rounded-pill fa-pull-right">
+            className="mt-2 me-2 btn btn-large btn-light border border-secondary fw-bolder rounded-pill fa-pull-right">
             Edit profile
           </Link>
           <button onClick={logout} className="mt-2 float-end btn btn-warning rounded-pill">
@@ -62,7 +64,7 @@ const Profile = () => {
             <a href="nasa.gov" className="text-decoration-none">nasa.gov</a>
             <i className="far fa-balloon ms-3 me-2"></i>
             Born October 1, 1958
-            <br/>
+            <br />
             <i className="far fa-calendar me-2"></i>
             Joined December 2007
           </p>
@@ -71,39 +73,47 @@ const Profile = () => {
           <ul className="mt-4 nav nav-pills nav-fill">
             <li className="nav-item">
               <Link to="/profile/mytuits"
-                    className={`nav-link ${location.pathname.indexOf('mytuits') >= 0 ? 'active':''}`}>
+                className={`nav-link ${location.pathname.indexOf('mytuits') >= 0 ? 'active' : ''}`}>
                 Tuits</Link>
             </li>
             <li className="nav-item">
               <Link to="/profile/tuits-and-replies"
-                    className={`nav-link ${location.pathname.indexOf('tuits-and-replies') >= 0 ? 'active':''}`}>
+                className={`nav-link ${location.pathname.indexOf('tuits-and-replies') >= 0 ? 'active' : ''}`}>
                 Tuits & replies</Link>
             </li>
             <li className="nav-item">
               <Link to="/profile/media"
-                    className={`nav-link ${location.pathname.indexOf('media') >= 0 ? 'active':''}`}>
+                className={`nav-link ${location.pathname.indexOf('media') >= 0 ? 'active' : ''}`}>
                 Media</Link>
             </li>
             <li className="nav-item">
               <Link to="/profile/mylikes"
-                    className="nav-link">
+                className="nav-link">
                 My Likes</Link>
             </li>
             <li className="nav-item">
               <Link to="/profile/mydislikes"
-                    className="nav-link">
+                className="nav-link">
                 My Dislikes</Link>
             </li>
-            
-             
+            {profile.role === "ADMIN" ? (
+              <li className="nav-item">
+                <Link to="/profile/allusers"
+                  className="nav-link">
+                  ADMIN</Link>
+              </li>
+            ) : null}
+
+
           </ul>
         </div>
       </div>
-        <Routes>
-          <Route path="/mytuits" element={<MyTuits/>}/>
-          <Route path="/mylikes" element={<MyLikes/>}/>
-          <Route path="/mydislikes" element={<MyDislikes/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/mytuits" element={<MyTuits />} />
+        <Route path="/mylikes" element={<MyLikes />} />
+        <Route path="/mydislikes" element={<MyDislikes />} />
+        <Route path="/allusers" element={<UserList />} />
+      </Routes>
     </div>
   );
 }
